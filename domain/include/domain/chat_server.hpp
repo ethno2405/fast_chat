9f72e1db156607d0bff2b4ee9048a35ecb32d94b
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2025-08-03 23:49:33 +0300

    ChatServer implementation

--- domain/include/domain/chat_server.hpp
+++ domain/include/domain/chat_server.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "domain/chat_room.hpp"
+#include "domain/password_hasher.hpp"
+
 #include <memory>
-#include <optional>
 #include <string>
 #include <vector>
 
@@ -12,16 +14,24 @@ class ChatRoom;
 
 class ChatServer {
   public:
+    ChatServer(std::shared_ptr<IPasswordHasher> hasher);
+
     std::shared_ptr<User> register_user(const std::string& username,
                                         const std::string& password);
     std::shared_ptr<User> login(const std::string& username,
                                 const std::string& password);
+    void logout(const std::string& username);
 
     std::shared_ptr<ChatRoom> find_room(const std::string& name) const;
     std::shared_ptr<ChatRoom> create_room(const std::string& name);
 
   private:
     std::vector<std::shared_ptr<ChatRoom>> rooms;
-    std::vector<std::shared_ptr<User>> users;
+    Users users;
+    Users online_users;
+    std::shared_ptr<IPasswordHasher> hasher;
+    bool is_user_online(const std::string& username) const;
+    bool user_exists(const std::string& username) const;
+    std::shared_ptr<User> find_user(const std::string& username) const;
 };
 } // namespace fast_chat
---
+++ domain/src/chat_server.cpp
@@ -0,0 +1,102 @@
+#include "domain/chat_server.hpp"
+#include "domain/chat_room.hpp"
+#include "domain/password_hasher.hpp"
+#include "domain/user.hpp"
+
+#include <assert.h>
+#include <memory>
+#include <string>
+
+namespace fast_chat {
+
+ChatServer::ChatServer(std::shared_ptr<IPasswordHasher> hasher)
+    : hasher(std::move(hasher)) {}
+
+std::shared_ptr<User> ChatServer::register_user(const std::string& username,
+                                                const std::string& password) {
+    assert(!username.empty());
+    assert(!password.empty());
+
+    if (user_exists(username)) {
+        return nullptr; // log user already exists
+    }
+
+    auto new_user = std::make_shared<User>(username, hasher->hash(password));
+    users.push_back(new_user);
+    return new_user;
+}
+
+std::shared_ptr<User> ChatServer::login(const std::string& username,
+                                        const std::string& password) {
+    assert(!username.empty());
+    assert(!password.empty());
+
+    auto user = find_user(username);
+    if (!user) {
+        return nullptr; // log user doesn't exists
+    }
+
+    if (is_user_online(user->get_username())) {
+        return nullptr; // log user is already logged in (online)
+    }
+
+    if (user->check_password(password, *hasher) == false) {
+        return nullptr; // log invalid password attempt
+    }
+
+    online_users.push_back(user);
+    return user;
+}
+
+void ChatServer::logout(const std::string& username) {
+    assert(!username.empty());
+
+    if (is_user_online(username) == false) {
+        return; // log user is offline
+    }
+
+    std::remove_if(online_users.begin(), online_users.end(),
+                   [&username](const std::shared_ptr<User>& user) {
+                       return user->get_username() == username;
+                   });
+}
+
+std::shared_ptr<ChatRoom> ChatServer::find_room(const std::string& name) const {
+    assert(!name.empty());
+
+    return *std::find_if(rooms.begin(), rooms.end(),
+                         [&name](const std::shared_ptr<ChatRoom>& room) {
+                             return room->get_name() == name;
+                         });
+}
+
+std::shared_ptr<ChatRoom> ChatServer::create_room(const std::string& name) {
+    assert(!name.empty());
+
+    if (std::any_of(rooms.begin(), rooms.end(),
+                    [&name](const std::shared_ptr<ChatRoom>& room) {
+                        return room->get_name() == name;
+                    })) {
+        return nullptr; // log room already exists
+    }
+
+    auto new_room = std::make_shared<ChatRoom>(name);
+    rooms.push_back(new_room);
+    return new_room;
+}
+
+bool ChatServer::user_exists(const std::string& username) const {
+    return std::any_of(users.begin(), users.end(),
+                       [&username](const std::shared_ptr<User>& user) {
+                           return user->get_username() == username;
+                       });
+}
+
+std::shared_ptr<User> ChatServer::find_user(const std::string& username) const {
+    return *std::find_if(users.begin(), users.end(),
+                         [&username](const std::shared_ptr<User>& user) {
+                             return user->get_username() == username;
+                         });
+}
+
+} // namespace fast_chat
