e6d431290b7b9e52539e534aa308f0861db93ce5
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2025-08-03 21:43:05 +0300

    User implementation

--- domain/src/user.cpp
+++ domain/src/user.cpp
@@ -1,6 +1,13 @@
-// #include "domain/user.hpp"
+#include "domain/user.hpp"
+#include "domain/password_hasher.hpp"
 
-// namespace fast_chat {
-// User::User(std::string username, std::string password_hash)
-//     : username(username), password_hash(password_hash) {}
-// } // namespace fast_chat
+namespace fast_chat {
+User::User(std::string username, std::string password_hash)
+    : username(std::move(username)), password_hash(std::move(password_hash)) {}
+
+const std::string& User::get_username() const { return username; }
+bool User::check_password(const std::string& password,
+                          const IPasswordHasher& hasher) const {
+    return hasher.verify(password_hash, password);
+}
+} // namespace fast_chat
